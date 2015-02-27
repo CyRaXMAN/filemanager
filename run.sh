6e1f93fecb2dfeb7bf30d66f57fb69ea19357cf4
CyRaXMAN <cyraxman0s@gmail.com>
2015-02-27 15:35:43 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ EXECUTABLE="main.py"
 SERVER_IP="0.0.0.0"
 SERVER_PORT="8000"
 
-$PYTHON $EXECUTABLE --server_ip=$SERVER_IP --server_port=$SERVER_PORT
+$PYTHON $EXECUTABLE --server_address=$SERVER_IP --server_port=$SERVER_PORT
